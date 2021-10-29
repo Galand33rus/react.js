1424b3f9848ff1e69d1cd3078f918f91a0ea4e7a
Galand33rus <galand_@mail.ru>
2021-10-29 11:32:39 +0300

    development

--- src/App.js
+++ src/App.js
@@ -1,19 +1,60 @@
-import logo from './logo.svg';
-import newLogo from '../src/assets/wolf.jpg';
-import './App.css';
-import { Message } from './components/Message/Message';
+import './App.scss';
+import {Message} from './components/Message/Message';
+import {useState, useEffect} from "react";
 
 function App() {
 
-  const message = "transmitted text";
+  const [value, setValue] = useState('');
+  const [message, setMessage] = useState({});
+  const [messageList, setMessageList] = useState([]);
+
+  const handleMessageChange = event => {
+    setValue(event.target.value)
+    setMessage({
+      author: 'User',
+      text: event.target.value
+    });
+  };
+
+  const updateMessage = event => {
+    event.preventDefault();
+    setMessageList(messageList => ([...messageList, message]));
+    setValue('');
+  };
+
+  useEffect(() => {
+    console.log(messageList);
+    // console.log(messageList[messageList.indexOf(message)]);
+  }, [messageList]);
+
+  useEffect(() => {
+    if (messageList[messageList.length - 1] && messageList[messageList.length - 1].author !== "Bot") {
+      setTimeout(() => {
+        setMessageList([
+          ...messageList, {
+            author: "Bot",
+            text: "I'm a bot"
+          }
+        ]);
+      }, 1500);
+    }
+  }, [messageList]);
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Message message={message} />
-        <img src={newLogo} className="wolf" alt="wolf" />
+        <h1 className="title">Chat</h1>
       </header>
+      <main className="App-main">
+        <dir className="chat">
+          <Message className="text-message" message={messageList}/>
+        </dir>
+        <form onSubmit={updateMessage}>
+          <input className="input" type="text" placeholder="enter a message" value={value}
+                 onChange={handleMessageChange}/>
+          <button className="button" type="submit" onSubmit={updateMessage}>send</button>
+        </form>
+      </main>
     </div>
   );
 }
--- src/components/Message/Message.jsx
+++ src/components/Message/Message.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import './Message.css';
+import './Message.scss';
 
-export const Message = ({ message }) => {
-  return (
-    <p className="message">
-      {message}
-    </p>
-  );
+export const Message = (props) => {
+  const messageList = props.message
+  return messageList.map((message) =>
+    <p className="message" key={messageList.indexOf(message)}>
+      <span className="author">{message.author}</span>: {message.text}
+    </p>);
 };
