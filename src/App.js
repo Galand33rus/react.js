b53a98cf8ff19a3cb2ad06c8d67be9aa90b4686d
Galand <galand_@mail.ru>
2021-11-02 14:06:05 +0300

    development

--- src/App.js
+++ src/App.js
@@ -1,33 +1,52 @@
 import './App.scss';
 import {Message} from './components/Message/Message';
-import {useState, useEffect} from "react";
+import {useState, useEffect, useRef} from "react";
 import {Form} from "./components/Form/Form";
+import {v4 as uuidv4} from 'uuid';
+import {AUTHORS} from "./utils/constants"
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
 
 function App() {
 
   const [messageList, setMessageList] = useState([]);
+  const [chatList, setChatList] = useState([
+    {
+    name: 'tempName',
+    id: uuidv4()
+    }
+  ]);
+  const scroll = useRef();
+  const scrollToBottom = () => {
+    // scroll.current?.scrollIntoView({behavior: "auto"});
+    scroll.current?.scrollIntoView(false);
+  }
 
   const updateMessageList = value => {
     setMessageList([...messageList, {
-      author: 'User',
-      text: value
+      author: AUTHORS.user,
+      text: value,
+      id: uuidv4()
     }]);
   };
 
   useEffect(() => {
-      const timer = setTimeout(() => {
-        if (messageList[messageList.length - 1] && messageList[messageList.length - 1].author !== "Bot") {
-          setMessageList([...messageList, {
-            author: "Bot",
-            text: "I'm a bot"
-          }
-          ]);
+    const timer = setTimeout(() => {
+      if (messageList[messageList.length - 1] && messageList[messageList.length - 1].author !== AUTHORS.bot) {
+        setMessageList([...messageList, {
+          author: AUTHORS.bot,
+          text: "I'm a bot",
+          id: uuidv4()
         }
-      }, 1500);
-
-      return () => clearTimeout(timer)
-
+        ]);
+      }
+    }, 1500);
+    return () => clearTimeout(timer)
   }, [messageList]);
+
+  useEffect(() => {
+    scrollToBottom();
+  });
 
 
   return (
@@ -36,10 +55,27 @@ function App() {
         <h1 className="title">Chat</h1>
       </header>
       <main className="App-main">
-        <dir className="chat">
-          <Message messageList={messageList}/>
-        </dir>
-        <Form updateMessageList={updateMessageList}/>
+        <div className="chat-list">
+          <p className="chat-list__title">list of chats</p>
+          <List>
+            <ListItem>
+              Chat # 1
+            </ListItem>
+            <ListItem>
+              Chat # 2
+            </ListItem>
+            <ListItem>
+              Chat # 3
+            </ListItem>
+          </List>
+        </div>
+        <div className="chat-wrapper">
+          <div className="chat" ref={scroll}>
+            <Message messageList={messageList}/>
+          </div>
+          <Form updateMessageList={updateMessageList}/>
+        </div>
+
       </main>
     </div>
   );
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,8 +1,26 @@
-import React, {useState} from 'react';
+import React, {useState, useRef} from 'react';
+import {TextField, Button} from "@mui/material";
+import {styled} from '@mui/material/styles';
+// import FormControl, {useFormControl} from '@mui/material/FormControl';
+// import {StyledEngineProvider} from '@mui/material/styles';
 import './Form.scss';
 
-export const Form = ({ updateMessageList }) => {
+export const Form = ({updateMessageList}) => {
   const [value, setValue] = useState('');
+  const inputRef = useRef();
+
+  // const theme = createTheme({
+  //   palette: {
+  //     primary: {
+  //       main: '#ff4400',
+  //     },
+  //     secondary: {
+  //       light: '#0066ff',
+  //       main: '#0044ff',
+  //       contrastText: '#ffcc00',
+  //     },
+  //   },
+  // });
 
   const handleMessageChange = event => {
     setValue(event.target.value)
@@ -13,22 +31,103 @@ export const Form = ({ updateMessageList }) => {
     if (value !== '') {
       updateMessageList(value);
       setValue('');
+      inputRef.current?.focus();
     }
   }
 
+  const CssTextField = styled(TextField)({
+    '& label.Mui-root': {
+      //color: '#2196f3',
+      color: 'rgba(255,255,255, .7)',
+    },
+    color: 'rgba(255,255,255, .7)',
+    '& label.Mui-focused': {
+      color: '#2196f3',
+      // color: 'red',
+    },
+    '& .MuiInput-underline:after': {
+      borderBottomColor: 'green',
+    },
+    '& .MuiOutlinedInput-root': {
+      color: 'rgba(255,255,255)',
+      '& fieldset': {
+        borderColor: '#2196f3',
+      },
+      '&:hover fieldset': {
+        borderColor: '#2196f3',
+        // boxShadow: '0 0 10px #2196f3'
+      },
+      '&.Mui-focused fieldset': {
+        borderColor: '#2196f3',
+      },
+    },
+  });
+
+  const StyledButton = styled(Button)({
+    width: 550,
+    color: '#fff',
+    marginTop: 20,
+    boxShadow: 'none',
+    textTransform: 'uppercase',
+    padding: '6px 12px',
+    border: '1px solid',
+    lineHeight: 1.5,
+    backgroundColor: 'transparent',
+    borderColor: '#2196f3',
+    '&:hover': {
+      backgroundColor: '#0069d9',
+      borderColor: '#2196f3',
+      boxShadow: 'none',
+    },
+    '&:active': {
+      boxShadow: 'none',
+      backgroundColor: '#2196f3',
+      borderColor: '#005cbf',
+    },
+  });
+
   return <form className="form" onSubmit={handleSubmit}>
-    <input
-      className="input"
-      type="text"
-      placeholder="Enter a message"
+    {/*<TextField*/}
+    {/*  id="outlined-basic"*/}
+    {/*  label="Enter a message"*/}
+    {/*  variant="outlined"*/}
+    {/*  autoComplete="off"*/}
+    {/*  autoFocus={true}*/}
+    {/*  value={value}*/}
+    {/*  ref={inputRef}*/}
+    {/*  margin="normal"*/}
+    {/*  color="primary"*/}
+    {/*  onChange={handleMessageChange}/>*/}
+
+    {/*<input*/}
+    {/*  className="input"*/}
+    {/*  type="text"*/}
+    {/*  placeholder="Enter a message"*/}
+    {/*  value={value}*/}
+    {/*  onChange={handleMessageChange}*/}
+    {/*  ref={inputRef}*/}
+    {/*  autoFocus={true}/>*/}
+    {/*<button*/}
+    {/*  className="button"*/}
+    {/*  type="submit">*/}
+    {/*    send*/}
+    {/*</button>*/}
+
+    <CssTextField
+      id="outlined-basic"
+      label="Enter a message"
+      autoComplete="off"
+      variant="outlined"
+      autoFocus={true}
       value={value}
-      onChange={handleMessageChange}
-      autoFocus={true}/>
-    <button
-      className="button"
-      type="submit">
-        send
-    </button>
+      ref={inputRef}
+      margin="normal"
+      color="primary"
+      onChange={handleMessageChange}/>
+
+    <StyledButton type="submit" margin="normal">send</StyledButton>
+
+
   </form>;
 };
 
